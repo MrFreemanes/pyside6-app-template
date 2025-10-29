7878f9f04283f75271454577a141c964b77f7b85
MrFreemanes <savazak3@gmail.com>
2025-10-29 16:00:57 +0300

    ADD: typification of results and tasks through dataclass, config.py

--- config/config.py
+++ config/config.py
@@ -1,5 +1,29 @@
+from dataclasses import dataclass
+from typing import Any
+
+
 class Status:
     """Используется в Bridge и в worker"""
     RUN = 'run'
     DONE = 'done'
     ERROR = 'error'
+
+
+@dataclass(repr=True)
+class Task:
+    """
+    Датакласс для передачи задач в worker.
+    """
+    task: str
+
+
+@dataclass(repr=True)
+class Result:
+    """
+    Датакласс для передачи результата включая ошибки.
+    :status: Status.
+    """
+    result: Any
+    status: str
+    progress: int
+    text_error: str = 'Ошибка'
