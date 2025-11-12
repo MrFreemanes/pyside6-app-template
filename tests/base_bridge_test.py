4e8c3433d477fc3cc529619c22712878b363ed9f
MrFreemanes <savazak3@gmail.com>
2025-11-12 19:27:56 +0300

    REMOVE: base_bridge_test.py. REFACTOR: bgidge_test.py

--- tests/base_bridge_test.py
+++
@@ -1,6 +0,0 @@
-from queue import Queue
-from unittest import TestCase, main
-from unittest.mock import Mock, patch
-
-from core.bridges.base_bridge import BaseBridge
-
--- tests/bgidge_test.py
+++ tests/bgidge_test.py
@@ -1,7 +1,8 @@
 from queue import Queue
 from unittest import TestCase, main
-from unittest.mock import Mock
+from unittest.mock import Mock, MagicMock
 
+from config.config import Result, Status
 from core.bridges.bridge import Bridge
 
 
@@ -9,35 +10,36 @@ class BridgeTest(TestCase):
     def setUp(self):
         self.bridge = Bridge(Queue(), Queue())
 
-        self.bridge.logger = Mock()
+        self.bridge.logger = MagicMock()
         self.bridge.done_signal = Mock()
         self.bridge.error_signal = Mock()
         self.bridge.process_signal = Mock()
 
     def test_handle_result(self):
         """Зависит от реализации."""
-        res_1 = {'status': 'run'}
-        res_2 = {'status': 'done'}
-        res_3 = {'status': 'pass'}
+        res_1 = Result(10, Status.RUN, 10)
+        res_2 = Result(100, Status.DONE, 100)
+        res_3 = Result(100, Status.ERROR, 100, text_error='ERR')
+        res_4 = Result(100, 'asd', 100)
+
+        # test status 'run'
         self.bridge._handle_result(res_1)
+        self.assertEqual(self.bridge.process_signal.emit.call_args[0][0], res_1)
+
+        # test status 'done'
         self.bridge._handle_result(res_2)
+        self.assertEqual(self.bridge.done_signal.emit.call_args[0][0], res_2)
+        self.assertEqual(self.bridge.logger.debug.call_args[0][0], 'Получены последние данные')
+
+        # test status 'error'
         self.bridge._handle_result(res_3)
+        self.assertEqual(self.bridge.error_signal.emit.call_args[0][0], res_3.text_error)
+        self.assertEqual(self.bridge.logger.debug.call_args[0][0], 'Получена ошибка')
 
-        # test run
-        self.assertEqual(self.bridge.process_signal.emit.call_args[0][0],
-                         res_1)
-
-        # test done
-        self.assertEqual(self.bridge.done_signal.emit.call_args[0][0],
-                         res_2)
-        self.assertEqual(self.bridge.logger.debug.call_args[0][0],
-                         'Получены последние данные')
-
-        # test error
-        self.assertEqual(self.bridge.error_signal.emit.call_args[0][0],
-                         f"Статус не определен: {res_3['status']}")
-        self.assertEqual(self.bridge.logger.warning.call_args[0],
-                         ('Статус не определен: %s', 'pass'))
+        # test non-existent status
+        self.bridge._handle_result(res_4)
+        self.assertEqual(self.bridge.error_signal.emit.call_args[0][0], f'Статус не определен: {res_4.status}')
+        self.assertEqual(self.bridge.logger.warning.call_args[0], ('Статус не определен: %s', res_4.status))
 
 
 if __name__ == '__main__':
