857da93c4d61c3c714353076dd6c282a8954c84b
MrFreemanes <savazak3@gmail.com>
2025-11-30 16:22:34 +0300

    STYLE: bridge_utils.py, widget_overrides.py

--- utils/bridge_utils.py
+++ utils/bridge_utils.py
@@ -1,5 +1,5 @@
 from typing import Any, Callable
-from config.config import Task
+from config.config import Task, TaskType
 
 
 def get_task_from_parameters(task_name: str, params: Any, *, task_type: str | None,
@@ -9,18 +9,14 @@ def get_task_from_parameters(task_name: str, params: Any, *, task_type: str | No
     if progress_handler is not None and not callable(progress_handler):
         raise ValueError("progress_handler must be callable or None")
 
-    # берем имена методов если они были переданы
-    done_name = done_handler.__name__ if callable(done_handler) else None
-    progress_name = progress_handler.__name__ if callable(progress_handler) else None
-
     task_kwargs = {
         'task_name': task_name,
         'params': params,
-        'progress_handler': progress_name,
-        'done_handler': done_name
+        'progress_handler': progress_handler.__name__ if callable(progress_handler) else None,
+        'done_handler': done_handler.__name__ if callable(done_handler) else None
     }
 
     if task_type:
-        task_kwargs["task_type"] = task_type
+        task_kwargs['task_type'] = task_type
 
     return Task(**task_kwargs)
